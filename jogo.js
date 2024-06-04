3981a4d97d18de2cf2ef7b4465f0ef64c101af54
Rodrigonovato10 <luisrodrigo.furtado123@gmail.com>
2024-06-04 17:50:25 -0300

    jogo do JokenPô

---
+++ jogo.js
@@ -0,0 +1,43 @@
+const result = document.querySelector('.result')
+const youScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
+
+
+
+const playHuman = (humanChoice) => {
+
+    playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNuber = Math.floor(Math.random() * 3)
+
+    return choices[randomNuber]
+}
+
+const playTheGame = (human, machine) => {
+
+    console.log('Humano:' + human + "Maquina: " + machine)
+
+    if (human === machine) {
+        result.innerHTML = "Deu empate!"
+
+    } else if(
+        (human === 'paper' && machine === 'rock') ||
+        (human === 'rock' && machine === 'scissors') ||
+        (human === 'scissors' && machine === 'paper')
+    ){
+        humanScoreNumber++
+        youScore.innerHTML = humanScoreNumber
+        result.innerHTML = "Você Ganhou!"
+    }
+    else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = "Você perdeu!"
+    }
+}
